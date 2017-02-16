850903d340af31c8bb86f92c85a319b29893de87
cinspire93 <cinspire93@gmail.com>
2017-02-15 20:00:23 -0800

    updated Kmeans

--- Kmeans.py
+++ Kmeans.py
@@ -8,8 +8,7 @@ class Kmeans(object):
     Expects rows in data to be data points, and columns to be different features
     '''
 
-    def __init__(self, max_iterations=10000, num_centroids=3, dist_metric='euclidean',
-                 smart_centroid_init=True):
+    def __init__(self, max_iterations=10000, num_centroids=3, dist_metric='euclidean'):
         '''
         Takes in a max number of iterations,
         number of clusters desired,
@@ -20,8 +19,7 @@ class Kmeans(object):
                             'manhattan': manhattan_distances}
         self.max_iterations = max_iterations
         self.num_centroids = num_centroids
-        self.smart = smart_centroid_init
-        self.centroids = []
+        self.centroids = None
         self.dist_mat = None
         self.assignments = None
         self._dist_func = self._dist_funcs[dist_metric]
@@ -39,25 +37,7 @@ class Kmeans(object):
         Takes in 2d np array data
         Sets a 2d np array of centroids randomly sampled from the data
         '''
-        if self.smart:
-            self._smart_centroid_init(data)
-        else:
-            self.centroids = np.array(random.sample(data, self.num_centroids))
-
-    def _smart_centroid_init(self, data):
-        '''
-        If the smart centroid init option is True,
-        Takes in 2d np array data
-        Sets centroids according to the Kmeans++ algorithm
-        '''
-        centroids_loc = []
-        sample_size = data.shape[0]
-        weights = np.array([1.0 / sample_size] * sample_size)
-        for i in xrange(self.num_centroids):
-            centroids_loc.append(data[np.random.choice(sample_size, p=weights)])
-            self.centroids = np.array(centroids_loc)
-            dist2 = np.min(self._dist_func(self.centroids, data)**2, axis=0)
-            weights = dist2 / np.sum(dist2)
+        self.centroids = np.array(random.sample(data, self.num_centroids))
 
     def _update_centroid(self, data):
         '''
